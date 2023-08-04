147d97374393ffd672ebe2c243cb8c230e5d93ba
Nirbhay Kwatra <nkwatra1@gmail.com>
2023-08-03 23:09:46 -0700

    Tested API functionality

--- asset-manager/main.py
+++ asset-manager/main.py
@@ -5,14 +5,11 @@ root = os.getcwd() + f"\show-directory"
 
 storage = pipe.Storage(root)
 
-custom_dir = pipe.Directory()
-custom_show = pipe.Show()
-
 show_flash = storage.create_show("The Flash")
 
 shot_opening = show_flash.create_shot("Opening")
 
-asset_suit = shot_opening.create_asset("suit")
+asset_suit = shot_opening.create_asset("suit", "prop")
 
 
 

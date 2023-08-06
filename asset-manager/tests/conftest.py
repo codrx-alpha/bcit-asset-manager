90181624b58806cc2dd3b1b553956cba7b1395ae
Nirbhay Kwatra <nkwatra1@gmail.com>
2023-08-06 03:11:30 -0700

    Add 'shot' fixture to conftest.py

--- asset-manager/tests/conftest.py
+++ asset-manager/tests/conftest.py
@@ -11,3 +11,10 @@ def storage(tmpdir) -> pipeline.Storage:
 def show(tmpdir) -> pipeline.Show:
     storage = pipeline.Storage(tmpdir.strpath)
     return storage.create_show("test-show")
+
+
+@pytest.fixture
+def shot(tmpdir) -> pipeline.Shot:
+    show = pipeline.Show(tmpdir.strpath)
+    return show.create_shot("test-shot")
+
